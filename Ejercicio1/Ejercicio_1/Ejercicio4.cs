3f298c2b44da0820a337bb4d5a182ba040cf9e4a
ZeroAndDonuts <gianmarco07042004@gmail.com>
2026-08-27 21:31:12 -0500

    Update Ejercicio4.cs

--- Ejercicio1/Ejercicio_1/Ejercicio4.cs
+++ Ejercicio1/Ejercicio_1/Ejercicio4.cs
@@ -4,6 +4,61 @@ namespace Ejercicio_1
 {
     internal class Ejercicio4
     {
-        
+        {
+    public static void Ejecutar()
+        {
+            string continuar;
+            {
+                Console.Write("Ingrese el primer número: ")
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el segundo número: ")
+                double num2 = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el tipo de operación (+, -, *, /, %): ");
+                string operacion = Console.ReadLine();
+
+                switch (operacion)
+                {
+                    case "+":
+                        Console.WriteLine("Resultado: " + (num1 + num2));
+                        break;
+
+                    case "-":
+                        Console.WriteLine("Resultado: " + (num1 - num2));
+                        break;
+
+                    case "*":
+                        Console.WriteLine("Resultado: " + (num1 * num2));
+                        break;
+
+                    case "/":
+                        if (numero2 != 0)
+                        {
+                            Console.WriteLine("Resultado: " + (num1 / num2));
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se puede dividir entre cero.");
+                        }
+                        break;
+
+                    case "%":
+                        if (numero2 != 0)
+                        {
+                            Console.WriteLine("Resultado: " + (num1 % num2));
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se puede dividir entre cero.");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("La operación no válida.");
+                        break;
+                }
+            }
+        }
     }
 }
