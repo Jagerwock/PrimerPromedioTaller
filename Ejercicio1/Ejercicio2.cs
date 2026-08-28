e035ef3275fac6313bf69799ad8408b7d106ca96
Atzel7 <axelrojasb@hotmail.com>
2026-08-27 19:05:57 -0500

    Ejercicio2

---
+++ Ejercicio1/Ejercicio2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Ejercicio2
+{
+    internal class triangulo
+    {
+        private float b;
+        private float h;
+
+        public triangulo(float b, float h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        public float CalcularArea()
+        {
+            return (b * h) / 2;
+        }
+    }
+}
