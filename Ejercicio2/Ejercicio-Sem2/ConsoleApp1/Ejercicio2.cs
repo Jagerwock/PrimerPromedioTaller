233dce6e870ee39e9cbd85d5560c53ca313ffcf0
andrius-cloud <guayando041@gmail.com>
2026-09-03 18:54:25 -0500

    Arreglos Ej2

--- Ejercicio2/Ejercicio-Sem2/ConsoleApp1/Ejercicio2.cs
+++ Ejercicio2/Ejercicio-Sem2/ConsoleApp1/Ejercicio2.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Ejercicio2 : Ejercicio1
+    internal class Ejercicio2 : Figura
     {
         public int Base;
         public int Altura;
