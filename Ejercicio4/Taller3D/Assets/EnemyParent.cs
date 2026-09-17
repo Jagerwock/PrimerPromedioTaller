244faa30188dbb57f9455bd7a0b3581c014178f1
Isma <ismaelesg0514@gmail.com>
2026-09-17 18:52:42 -0500

    Enemy parent push

---
+++ Ejercicio4/Taller3D/Assets/EnemyParent.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EnemyParent : MonoBehaviour
+{
+    public int enemyHealth;
+    public int enemyDamage;
+    
+    public void Start()
+    {
+        //texto de funcionamiento acia
+    }
+
+}
